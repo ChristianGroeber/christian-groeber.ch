fdaaab8dded16797d1db9020ca8907208efa9ae2
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-11-05 10:35:07 +0100

    initial Commit

---
+++ christian-groeber.ch/christian-groeber.ch/www/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace christian_groeber.ch
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
