3c4e8a344f30f19ee0a559c3fd69861c39d07d66
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-11-13 16:23:41 +0100

    Did the ground work for the projects page

---
+++ christian-groeber2.ch/christian-groeber2.ch/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace christian_groeber2.ch.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Project
+        public ActionResult PreciousHands()
+        {
+            return View();
+        }
+    }
+}
